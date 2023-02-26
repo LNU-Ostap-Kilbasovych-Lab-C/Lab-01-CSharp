d008bfb8342a25338cc3e6fe339d9fdc60c1a417
ostapkilbasovych <ostapkilbasovych@gmail.com	ostapkilbasovych@MacBook-Pro-Ostap ~ %  git config --global user.email ostapkilbasovych@gmail.com>
2023-02-26 12:14:30 +0200

    Initial commit Task 1

---
+++ Comput_practice_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Variant 15
+Task1(); // 1 -> 1
+Task1(); // 8 -> 10
+Task1(); // 125 -> 175
+Task1(); // 91201 -> 262101
+
+// Task1
+void Task1()
+{
+    Console.Write("Enter a natural number: ");
+    string userInput = Console.ReadLine();
+    uint userNumber = uint.Parse(userInput);
+    string octalNumber = "";
+
+    while (userNumber > 0)
+    {
+        uint remainder = userNumber % 8;
+        octalNumber = remainder.ToString() + octalNumber;
+        userNumber = userNumber / 8;
+    }
+
+    Console.WriteLine("The octal representation of " + userInput + " is " + octalNumber);
+}
+
